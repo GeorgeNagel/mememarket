6a75ea7b985237154bd45814c7f1418640cbfea8
George Nagel <gsnagel@gmail.com>
2016-11-26 20:25:51 -0500

    Connecting to PG and feeling good about it

--- main.go
+++ main.go
@@ -2,16 +2,33 @@ package main
 
 import (
 	"fmt"
+	"github.com/jackc/pgx"
 	"html"
 	"log"
 	"net/http"
 )
 
 func main() {
+	config := pgx.ConnConfig{
+		Host:     "localhost",
+		Database: "mememarket",
+		User:     "postgres",
+	}
+	conn, err := pgx.Connect(config)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("IT WORKED")
+
+	var memeCount int
+	err = conn.QueryRow("SELECT COUNT(*) FROM memes").Scan(&memeCount)
+	fmt.Printf("Memes: %d\n", memeCount)
+}
+
+func server() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
-
 }
---
+++ schema.sql
@@ -0,0 +1,165 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.6.1
+-- Dumped by pg_dump version 9.6.1
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner:
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner:
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: accounts; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE accounts (
+    username text NOT NULL,
+    id bigint NOT NULL
+);
+
+
+ALTER TABLE accounts OWNER TO postgres;
+
+--
+-- Name: accounts_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE accounts_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE accounts_id_seq OWNER TO postgres;
+
+--
+-- Name: accounts_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE accounts_id_seq OWNED BY accounts.id;
+
+
+--
+-- Name: accounts_memes_map; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE accounts_memes_map (
+    account_id integer NOT NULL,
+    meme_id integer NOT NULL,
+    amount integer NOT NULL
+);
+
+
+ALTER TABLE accounts_memes_map OWNER TO postgres;
+
+--
+-- Name: memes; Type: TABLE; Schema: public; Owner: postgres
+--
+
+CREATE TABLE memes (
+    name text NOT NULL,
+    price integer NOT NULL,
+    id bigint NOT NULL
+);
+
+
+ALTER TABLE memes OWNER TO postgres;
+
+--
+-- Name: memes_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
+--
+
+CREATE SEQUENCE memes_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE memes_id_seq OWNER TO postgres;
+
+--
+-- Name: memes_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
+--
+
+ALTER SEQUENCE memes_id_seq OWNED BY memes.id;
+
+
+--
+-- Name: accounts id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY accounts ALTER COLUMN id SET DEFAULT nextval('accounts_id_seq'::regclass);
+
+
+--
+-- Name: memes id; Type: DEFAULT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY memes ALTER COLUMN id SET DEFAULT nextval('memes_id_seq'::regclass);
+
+
+--
+-- Name: accounts accounts_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY accounts
+    ADD CONSTRAINT accounts_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: memes memes_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY memes
+    ADD CONSTRAINT memes_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: accounts_memes_map account_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY accounts_memes_map
+    ADD CONSTRAINT account_fk FOREIGN KEY (account_id) REFERENCES accounts(id) ON DELETE CASCADE;
+
+
+--
+-- Name: accounts_memes_map meme_fk; Type: FK CONSTRAINT; Schema: public; Owner: postgres
+--
+
+ALTER TABLE ONLY accounts_memes_map
+    ADD CONSTRAINT meme_fk FOREIGN KEY (meme_id) REFERENCES memes(id) ON DELETE CASCADE;
+
+
+--
+-- PostgreSQL database dump complete
+--
